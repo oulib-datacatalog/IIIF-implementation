8dda5a034b66da0aa1524942e7618f9cec81b791
Tao Zhao <tao.zhao.lib@gmail.com>
2018-04-02 20:37:29 +0000

    first working manifest and iiif.php

--- iiif.php
+++ iiif.php
@@ -8,11 +8,60 @@
     	header('Location: index.php'); 
     	exit();
     }
+    	
+    if(preg_match('/^\/iiif\/([a-z0-9]+)\/([0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12})\/(.*)$/', $uri, $matches)){
+        if(is_array($matches) && count($matches) === 4){
+                $entity_type = $matches[1];
+                $id = $matches[2];
+                $info_type = $matches[3];
+                if(preg_match('/^(.+)\/(.+)\/(\!?[0-9]*\.?[0-9]*)\/(color|gray|bitonal|default)\.(jpg|tif|png|gif|jp2|pdf|webp)$/', $info_type, $sub_matches)){
+                header("Location: http://172.28.128.4/fcgi-bin/iipsrv.fcgi?FIF=/usr/share/nginx/html/images/test2.tif&WID=full&CVT=jpeg");        
+		exit();
+                }
+                else{
+                        $collection_manifest_data = getCollectionManifestData($id);
+                        if($info_type === 'manifest'){
+                                header('Content-type: application/json');
+                                echo $collection_manifest_data;
+                                exit();
+                        }
+                        elseif(preg_match('/^(image|thumbnail)\/info$/', $info_type, $sub_matches)){
+                                print_r($sub_matches);
+                                echo getEntityObjectPath($id);
+                                exit();
+                        }
+                        elseif (preg_match('/^(sequence|canvas)\/([0-9a-z]+)$/', $info_type, $sub_matches)) {
+                        	$property_name = $sub_matches[1];
+                        	$property_id = $sub_matches[2];
+                        	$collection_manifest_data_obj = json_decode($collection_manifest_data);
+                        	header('Content-type: application/json');
+                            switch ($property_name) {
+                            	case 'sequence':
+                            		echo getSequenceData($collection_manifest_data_obj, $property_id);
+                            		break;
 
-    if(preg_match('/^\/iiif\/([a-z0-9]+)\/[a-f0-9]{8}\-[a-f0-9]{4}\-4[a-f0-9]{3}\-(8|9|a|b)[a-f0-9]{3‌​}\-[a-f0-9]{12}\//'), $uri, $matches){
-    	print_r($matches);
-    	exit();
+                        		case 'canvas':
+                        			echo getCanvasData($collection_manifest_data_obj, $property_id);
+                        			break;
+                            	
+                            	default:
+                            		# code...
+                            		break;
+                            }
+                                exit();
+                        }
+                }
+
+        }
+        else{
+                throw new Exception("Cannot match the URI: $uri.");
+        }
+        // print_r($matches);
+        echo " not further matches";
+        exit();
     }
+
+    echo "Not matches"; exit();
  
     if(preg_match('/^\/iiif\/collections\/([a-z0-9]+)\/(thumbnail|manifest|(sequences\/([a-z0-9]+))|(canvases\/([a-z0-9]+))|(images\/([a-z0-9]+)\/info))$/', $uri, $matches)){
     	$req_type = $matches[0];
@@ -126,6 +175,33 @@
     	$data = file_get_contents($manifest_path);
     	return $data;
     }
+
+    function getEntityObjectPath($id){
+    	return ROOT_PATH."$id/OBJ.tif";
+    }
+
+    function getSequenceData($collection_manifest_data_obj, $sequence_id){
+    	$requested_data_array = $collection_manifest_data_obj->sequences; 
+		foreach ($requested_data_array as $key => $value) {				
+			$id_value = $value->{'@id'};
+			if(preg_match('/\/(sequences\/'.$sequence_id.')$/', $id_value, $id_matches)){
+				return json_encode($value);
+			}
+		}
+    }
+
+    function getCanvasData($collection_manifest_data_obj, $canvas_id){
+    	$sequence_data_array = $collection_manifest_data_obj->sequences;
+		foreach ($sequence_data_array as $sequence_key => $sequence_value) {
+			$canvases = $sequence_value->canvases;
+			foreach ($canvases as $canvas_key => $canvas_value) {							
+				$id_value = $canvas_value->{'@id'};
+				if(preg_match('/\/(canvases\/'.$canvas_id.')$/', $id_value, $id_matches)){
+					return json_encode($canvas_value);
+				}
+			}
+		}
+    } 
 
     function getImageInfoPath($collection_id, $image_id){
     	$image_info_path = ROOT_PATH."$collection_id/$image_id/info.json";
